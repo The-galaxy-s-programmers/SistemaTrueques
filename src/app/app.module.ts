a3f77a01e3041ec843a98b53f368bb9f05af563e
JrManiac <69601774+JrManiac@users.noreply.github.com>
2020-08-31 16:56:55 -0400

    Add new files
    Nuevos archivos para compartir trabajo

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MenuProductosComponent } from './components/menu-productos/menu-productos.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuProductosComponent,
+    ProductoComponent,
+    RegistroUsuarioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/menu-productos/menu-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-productos',
+  templateUrl: './menu-productos.component.html',
+  styleUrls: ['./menu-productos.component.css']
+})
+export class MenuProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
