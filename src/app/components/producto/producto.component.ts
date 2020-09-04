444eba9be799e0fffa51ed54f863f0c2e67eb9ba
cami-code <camila.castro.palma@gmail.com>
2020-09-04 08:58:06 -0400

    update

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -13,7 +13,7 @@ export class ProductoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-descripcion="Juego de Comedor para 4 personas, poco uso, material reciclado manufactura chilena.";
+descripcion=" Mueble de cocina en excelente estado. ";
 valor="Valor Aprox: $75.000";
 nombre:string;
 correo:string;
